627b2ed5619d196f672b73267ae91bfe5c9aa342
Lý Mạnh Phi <lmanhphi01@gmail.com>
2024-03-17 14:39:47 +0700

    card: flip card

--- lib/study/word/word.dart
+++ lib/study/word/word.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flip_card/controllers/flip_card_controllers.dart';
+import 'package:flutter_flip_card/flipcard/flip_card.dart';
+import 'package:flutter_flip_card/modal/flip_side.dart';
 
-import '../../constant/style.dart';
-
-class WordItem extends StatelessWidget {
+class WordItem extends StatefulWidget {
   final String word;
   final String definition;
 
@@ -12,35 +13,72 @@ class WordItem extends StatelessWidget {
   });
 
   @override
+  State<WordItem> createState() => _WordItemState();
+}
+
+class _WordItemState extends State<WordItem> {
+  final flip = FlipCardController();
+
+  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         print("object");
       },
-      child: Card(
-        color: Color.fromARGB(255, 0, 191, 255),
-        elevation: 10,
-        margin: const EdgeInsets.all(8),
-        child: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  word,
-                  style: const TextStyle(fontSize: 18.0, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 8), // Adjust the spacing between word and definition
-                Text(
-                  definition,
-                  style: const TextStyle(fontSize: 18.0, color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            ),
+      child: FlipCard(
+        controller: flip,
+        onTapFlipping: true,
+        rotateSide: RotateSide.bottom,
+        frontWidget: _buildFront(),
+        backWidget: _buildBack(),
+      ),
+    );
+  }
+
+  Widget _buildFront() {
+    return Card(
+      key: ValueKey('front'),
+      color: Color.fromARGB(255, 0, 191, 255),
+      elevation: 10,
+      margin: const EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.word,
+                style: const TextStyle(fontSize: 35, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBack() {
+    return Card(
+      key: ValueKey('back'),
+      color: Color.fromARGB(255, 0, 191, 255),
+      elevation: 10,
+      margin: const EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.definition,
+                style: const TextStyle(fontSize: 35, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ],
           ),
         ),
       ),
